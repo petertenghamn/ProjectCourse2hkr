3deede073a8bf754d5ac9b5b832b8aa7d4f0e266
Peter Tenghamn <petertenghamn@hotmail.com>
2019-04-21 23:30:32 +0200

    DB test connection

---
+++ src/Main/DatabaseLoader.java
@@ -0,0 +1,36 @@
+package Main;
+
+import java.sql.*;
+
+public class DatabaseLoader {
+
+    String url = "jdbc:mysql://127.0.0.1:3306/pokedb?user=root&password=root";
+    Statement statement;
+
+    private boolean connected;
+
+    public DatabaseLoader(){
+        try {
+            Connection c = (Connection) DriverManager.getConnection(url);
+            statement = c.createStatement();
+            connected = true;
+        } catch (SQLException ex){
+            System.out.println("DB-Connection failed");
+            connected = false;
+        }
+    }
+
+    public void TestFunction(){
+        if (connected) {
+            try {
+                ResultSet rs = statement.executeQuery("SELECT name FROM pokemon");
+
+                while (rs.next()) {
+                    System.out.println("Pokemon name: " + rs.getString(1));
+                }
+            } catch (SQLException ex) {
+                System.out.println("Error executing the query!");
+            }
+        }
+    }
+}
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -8,14 +8,19 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    DatabaseLoader pokeDB;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
+        //create connection with server
+        pokeDB = new DatabaseLoader();
+        pokeDB.TestFunction();
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
