cecf37ef4c31970ab9a943ab1be33cfa07f97446
Peter Tenghamn <petertenghamn@hotmail.com>
2019-04-22 19:54:30 +0200

    Connection made

--- src/Main/DatabaseLoader.java
+++ src/Main/DatabaseLoader.java
@@ -7,29 +7,52 @@ public class DatabaseLoader {
     String url = "jdbc:mysql://127.0.0.1:3306/pokedb?user=root&password=root";
     Statement statement;
 
+    private Connection con;
     private boolean connected;
 
-    public DatabaseLoader(){
+    public void TestFunction(){
         try {
-            Connection c = (Connection) DriverManager.getConnection(url);
-            statement = c.createStatement();
+            con = (Connection) DriverManager.getConnection(url);
+            statement = con.createStatement();
             connected = true;
         } catch (SQLException ex){
             System.out.println("DB-Connection failed");
             connected = false;
         }
-    }
 
-    public void TestFunction(){
         if (connected) {
+            //create a pokemon to print
             try {
-                ResultSet rs = statement.executeQuery("SELECT name FROM pokemon");
+                statement.executeUpdate("INSERT into pokemon values (1, 'ElectroRat', 9001, 999, 999, 1000);");
+            } catch (SQLException ex) {
+                System.out.println("Error executing the update - insert!");
+            }
+
+            //print out test pokemon
+            try {
+                ResultSet rs = statement.executeQuery("SELECT name FROM pokemon;");
 
                 while (rs.next()) {
                     System.out.println("Pokemon name: " + rs.getString(1));
                 }
             } catch (SQLException ex) {
                 System.out.println("Error executing the query!");
+            }
+
+            //remove pokemon made
+            try {
+                statement.executeUpdate("Delete from pokemon where pokemon_id = 1;");
+            } catch (SQLException ex) {
+                System.out.println("Error executing the update - delete!");
+            }
+
+            if (connected){
+                try {
+                    con.close();
+                }
+                catch (Exception e){
+                    System.out.println("DB-Connection closing exception");
+                }
             }
         }
     }
