6dd3e095bc452e57fb9525e8b47198c666c25c93
Vik <viktor.westlund0011@stud.hkr.se>
2019-04-23 14:27:31 +0200

    added login screen (controller, FXML-files and css)

---
+++ src/sample/LoginController.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginController {
+    @FXML
+    Button btnLogin;
+    @FXML
+    Button btnCreateNA;
+    @FXML
+    TextField txtFieldUsername;
+    @FXML
+    TextField txtFieldPassword;
+    @FXML
+    AnchorPane anc;
+    @FXML
+    ImageView imgView;
+}
