1a42a96a238d86dffe2f1116f451b058fead1a40
Peter Tenghamn <petertenghamn@hotmail.com>
2019-05-09 17:53:44 +0200

    Revert "View All Collection"

--- src/scenes/TrainerMenuController.java
+++ src/scenes/TrainerMenuController.java
@@ -1,14 +1,9 @@
 package scenes;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.geometry.Orientation;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import main.Main;
-import pokemon.Pokemon;
 import pokemon.PokemonMapper;
 import users.Trainer;
 import users.User;
@@ -18,13 +13,10 @@ import java.util.*;
 public class TrainerMenuController implements Controller {
 
     @FXML
-    Label lblTop;
+    Button btnCollection;
 
     @FXML
-    Button btnCollection, btnAddPokemon, btnSelect;
-
-    @FXML
-    ListView<String> lvCollection ;
+    ListView listView;
 
     @Override
     public void setMain(Main m){
@@ -38,33 +30,6 @@ public class TrainerMenuController implements Controller {
 
     private Main main;
 
-
-    // This method should be hidden behind the "buy" feature later on
-    public void showAllPokemon(){
-        // Re-Arranges the scene to the selecting a new pokemon
-        btnSelect.setVisible(true);
-        lblTop.setText("Here are the Available Pokemons!");
-
-        lvCollection.setOrientation(Orientation.VERTICAL);
-
-        Pokemon[] pokemons = main.getAllPokemon();
-        ArrayList<String> tempNameString = new ArrayList<>();
-
-        for (int showAll = 0; showAll < pokemons.length; showAll++){
-            tempNameString.add(pokemons[showAll].getName());
-        }
-
-        // Observable Lists are the only list available to be used by the ListView so...
-        ObservableList<String>  pokemonNames = FXCollections.observableArrayList(tempNameString);
-
-        lvCollection.getItems().addAll(pokemonNames);
-    }
-
-    // Opens the Scene AcquirePokemon
-    public void boughtPokemon(){
-
-    }
-
     public void showPokemonCollection(){
         User user = main.getCurrentUser();
         /*
@@ -74,11 +39,7 @@ public class TrainerMenuController implements Controller {
             for (PokemonMapper poke : collection){
                 pokeNames.add(main.getPokemon(poke.getId()).getName());
             }
-
-            // Needed for a ListView in JavaFX for some reason
-            ObservableList<String> collectionPokemon = FXCollections.observableArrayList(pokeNames);
-
-            lvCollection.setItems(collectionPokemon);
+            listView.getItems().setAll(pokeNames);
         }
         */
     }
