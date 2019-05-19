8fd710df33ad7c61d9e90223f0677d3abcd56dbf
Ioana <ioana.parosu0003@stud.hkr.se>
2019-05-19 20:17:28 +0200

    error handling
    added labels to the login screen to show errors when the field is empty or the info incorrect, made impossible for the user to leave an empty nickname for his new pokemon and hid the button to buy in the professor  screen

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,7 @@
 package main;
 
 //not importing database loader caused errors... not sure why...
+
 import javafx.scene.image.Image;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -25,11 +26,18 @@ public class Main extends Application {
     private Pokemon[] allPokemon;
 
     /*
+     * Main method, Java startup
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /*
      * Get the full list of all pokemon currently stored
      *
      * @returns Pokemon[]
      */
-    public Pokemon[] getAllPokemon(){
+    public Pokemon[] getAllPokemon() {
         return allPokemon;
     }
 
@@ -38,9 +46,9 @@ public class Main extends Application {
      *
      * @returns Pokemon
      */
-    public Pokemon getPokemonById(int id){
-        for (Pokemon pID : allPokemon){
-            if (pID.getIdTag() == id){
+    public Pokemon getPokemonById(int id) {
+        for (Pokemon pID : allPokemon) {
+            if (pID.getIdTag() == id) {
                 return pID;
             }
         }
@@ -53,9 +61,9 @@ public class Main extends Application {
      *
      * @returns Pokemon
      */
-    public Pokemon getPokemonByName(String name){
-        for (Pokemon pN: allPokemon){
-            if(pN.getName().equalsIgnoreCase(name)) {
+    public Pokemon getPokemonByName(String name) {
+        for (Pokemon pN : allPokemon) {
+            if (pN.getName().equalsIgnoreCase(name)) {
                 return pN;
             }
         }
@@ -82,23 +90,16 @@ public class Main extends Application {
     }
 
     /*
-     * Main method, Java startup
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    /*
      * Change the scene to the requested scene
      */
-    public void requestSceneChange(SceneManager.sceneName scene){
+    public void requestSceneChange(SceneManager.sceneName scene) {
         manager.changeScene(scene);
     }
 
     /*
      * Logout the user and return to login screen
      */
-    public void logoutUser(){
+    public void logoutUser() {
         if (currentUser != null) {
             currentUser = null;
         }
@@ -109,19 +110,18 @@ public class Main extends Application {
      * Calls to the DB to verify the information entered is correct
      * Only changes screen if information is matched to a user in the DB
      */
-    public void authenticateLogin(String email, String password){
+    public boolean authenticateLogin(String email, String password) {
         currentUser = pokeDB.authenticateLogin(email, password);
         if (currentUser != null) {
-            if (currentUser instanceof Trainer){
+            if (currentUser instanceof Trainer) {
                 manager.changeScene(SceneManager.sceneName.TRAINERMENU);
                 System.out.println("Trainer deserves a login bonus: " + pokeDB.loginBonusCheck(email));
-            }
-            else if (currentUser instanceof Professor){
+            } else if (currentUser instanceof Professor) {
                 manager.changeScene(SceneManager.sceneName.PROFESSORMENU);
             }
-        }
-        else {
-            System.out.println("Information entered was incorrect!");
+            return true;
+        } else {
+            return false;
         }
     }
 
@@ -129,15 +129,14 @@ public class Main extends Application {
      * Check that the email is unique then create a new user class with entered information
      * This does not add the user to the DB yet as the creation can still be canceled
      */
-    public void createNewUser(String email, String username, String password){
+    public void createNewUser(String email, String username, String password) {
         //creates a new user class of trainer to store info in, then transitions to selecting a starter
         if (pokeDB.checkIfEmailAvailable(email)) {
             currentUser = new Trainer(email, username, 0, 0, 0, new PokemonMapper[0], new PokemonMapper[0]);
             ((Trainer) currentUser).setNewUserPassword(password);
 
             manager.changeScene(SceneManager.sceneName.SELECTSTARTER);
-        }
-        else {
+        } else {
             System.out.println("Email already in use!");
         }
     }
@@ -145,7 +144,7 @@ public class Main extends Application {
     /*
      * Assign the selected pokemon to the new trainer and finalize by adding the trainer to the DB
      */
-    public void acquirePokemon(int pokemonID, String nickname){
+    public void acquirePokemon(int pokemonID, String nickname) {
         ArrayList<PokemonMapper> collection = ((Trainer) currentUser).getCollection();
 
         //cannot have a null name when inserting into DB
@@ -153,21 +152,20 @@ public class Main extends Application {
             nickname = getPokemonById(pokemonID).getName();
         }
 
-        for (PokemonMapper mappedPokemon : collection){
+        for (PokemonMapper mappedPokemon : collection) {
             // Checks if there are pokemon with that nickname in your collection already
             if (!mappedPokemon.getNickname().equalsIgnoreCase(nickname)) {
                 //attach the selected starter to the trainer that choose it, then proceed to trainer menu
                 PokemonMapper caughtPokemon = new PokemonMapper(pokemonID, nickname);
                 ((Trainer) currentUser).addToCollection(caughtPokemon);
                 break; // Needed or else it breaks the for loop
-            }
-            else {
+            } else {
                 System.out.println("There is already a Pokemon with that name in your collection: " + nickname);
             }
         }
 
         // Later will move this part of the code
-        if (((Trainer) currentUser).getCollection().size() == 0){
+        if (((Trainer) currentUser).getCollection().size() == 0) {
             pokeDB.createNewUser(currentUser);
             manager.changeScene(SceneManager.sceneName.TRAINERMENU);
         }
@@ -178,13 +176,13 @@ public class Main extends Application {
      *
      * @returns Image
      */
-    public Image getPokemonImage(String pokemonName){
+    public Image getPokemonImage(String pokemonName) {
         // Set default image, can't return null if the pokemon image is missing
         Image image = new Image("scenes/view/images/pokeLogo.png");
 
         try {
             image = new Image("scenes/view/images/" + getPokemonByName(pokemonName).getName().toLowerCase() + ".png");
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println("Missing image for - " + getPokemonByName(pokemonName).getName().toLowerCase());
         }
 
